621f8be0bb9b52bda2a0457f8d60a7ca7126b67c
Sam Phillips <sam@phillips.org>
2015-12-17 20:52:50 -0600

    Initial Check in

---
+++ ex22.py
@@ -0,0 +1,37 @@
+print	writes to the standard out
++	plus
+-	minus
+/	divide
+*	multiply
+%	percent
+<	less than
+>	greater than
+<=	less than or equal to
+>=	greater than or equal to
+%r	format defintion for string
+%s	format definiton for raw data
+%d	format defintion for numbers
+raw_input	reading from standard in
+\t	tab in a string
+\n	return character
+\\	backslash in a string
+from	keyword indicating library to import from
+import	keyword indicating function to import
+argv	function from sys library also returns command line as a list
+sys		library
+"""  	multiline string
+open	function to open file
+close	closes file
+read	    reads the contents of the file
+readline	reads just on line of a text file
+tuncate		empties the file
+write('stuff')		writes "stuff" to the file
+exists		function that tells you whether a file exists
+def		    define a function
+return		return value of a function
+for
+in			inside a file
+if 
+if not
+else
+try
